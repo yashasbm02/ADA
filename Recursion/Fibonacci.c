6bd891dbf685bcf90b89884873598071726a8864
Yashas B Manjunath <76400144+yashasbm02@users.noreply.github.com>
2022-08-12 22:26:08 +0530

    Memoization to make it O(n)

--- Recursion/Fibonacci.c
+++ Recursion/Fibonacci.c
@@ -1,24 +1,30 @@
-//Program to find the fibonnaci series of a given limit
+//Program to find the fibonnaci series of a given limit using dynamic programming
 #include<stdio.h>
-
+int term[1000];//Memoization array
+int count=0;
 //Function code
-int fib(int n,int mem){
-    if(mem[n]!=NULL){
-        return mem[n]!
-    if(n==1||n==0){
-        return 1;
+int fib(int n){
+    count++;
+    if(term[n]!=0){
+        return term[n];
+    }
+    if(n<=1){
+        return n;
     }
     else{
-        int result=fib(n-1)+fib(n-2);//Recursion call
-        mem[n]=result;
-    }
+        term[n]=fib(n-1)+fib(n-2);
+        return term[n];//Recursion call
+        }
 }
+
+
 
 //Driver code
 int main(){
     int n;
     printf("Enter the fibonacci limit\n");
     scanf("%d",&n);
-    printf("%d th element of the series is: %d",n,fib(n));
+    printf("%d th element of the series is: %d\n",n,fib(n));
+    printf("count is %d\n",count);
     return 0;
 }
